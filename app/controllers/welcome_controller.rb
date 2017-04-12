582ff8b5d3c39ca338e3beee024fe7a80657e618
Steve Licata <Steve.Licata@gmail.com>
2017-04-11 23:52:24 -0400

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+
+  def home
+	   @message = "Welcome to Devise/Paperclip."
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  #paperclip
+  has_attached_file :avatar,
+    :styles => { :medium => "300x300px>", :thumb => "100x100px>" },
+    :default_url => "/assets/default_picture.png"
+
+  validates_attachment_content_type :avatar,
+    :content_type => /\Aimage\/.*\Z/
+
+end
