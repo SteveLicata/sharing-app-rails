75fbf32879941a4353c66ada2b5615193704591e
Steve Licata <Steve.Licata@gmail.com>
2017-09-13 16:24:27 -0400

    Posts MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911225331) do
+ActiveRecord::Schema.define(version: 20170913201559) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
